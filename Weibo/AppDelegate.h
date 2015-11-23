e3d7ef4865915f54b84d57999ae4b95b142fa869
XieTing <86755838@qq.com>
2015-11-23 10:31:27 +0800

    Initial Commit

---
+++ Weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weibo
+//
+//  Created by Mac on 15-11-23.
+//  Copyright (c) 2015年 XieTing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
