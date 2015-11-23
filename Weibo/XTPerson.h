7fb5fc675359012035804fb7c4097f7093749fd1
XieTing <86755838@qq.com>
2015-11-23 10:36:04 +0800

    添加了Person类

---
+++ Weibo/XTPerson.h
@@ -0,0 +1,13 @@
+//
+//  XTPerson.h
+//  Weibo
+//
+//  Created by Mac on 15-11-23.
+//  Copyright (c) 2015年 XieTing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XTPerson : NSObject
+
+@end
---
+++ Weibo/XTPerson.m
@@ -0,0 +1,13 @@
+//
+//  XTPerson.m
+//  Weibo
+//
+//  Created by Mac on 15-11-23.
+//  Copyright (c) 2015年 XieTing. All rights reserved.
+//
+
+#import "XTPerson.h"
+
+@implementation XTPerson
+
+@end
